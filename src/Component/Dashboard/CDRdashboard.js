45fd89c8bd6e7e81e46e1c2ae9b94963f2e5a3d9
Raushan kumar giri <raushan.giri@orangecurrent.com>
2024-09-20 06:27:08 +0100

    updated dashboard component

--- src/Component/Dashboard/CDRdashboard.js
+++ src/Component/Dashboard/CDRdashboard.js
@@ -6,50 +6,7 @@ const CDRdashboard = () => {
     {/* <Navbar/> */}
     {/* <p>Welcome back Admin</p> */}
     <div className="row">
-  <div className="col-sm-4 mb-3 mb-3">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">MTD- Total Intrested</h5>
-        <p className='fs-4'>10</p>
-      </div>
-    </div>
-  </div>
-  <div className="col-sm-4 mb-3 mb-3">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">MTD- Total Not-Intrested</h5>
-        <p className='fs-4'>10</p>
-
-      </div>
-    </div>
-  </div>
-  <div className="col-sm-4 mb-3">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">MTD-Total Pending TVR</h5>
-        <p className='fs-4'>10</p>
-
-      </div>
-    </div>
-  </div>
-  <div className="col-sm-4 mb-3">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">MTD-Total Completed TVR</h5>
-        <p className='fs-4'>10</p>
-
-      </div>
-    </div>
-  </div>
-  <div className="col-sm-4 mb-3">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">MTD-Total Rejected TVR</h5>
-        <p className='fs-4'>10</p>
-
-      </div>
-    </div>
-  </div>
+  
   <div className="col-sm-4 mb-3">
     <div className="card">
       <div className="card-body">
--- src/Component/Dashboard/tvr_dashboard.js
+++ src/Component/Dashboard/tvr_dashboard.js
@@ -7,23 +7,6 @@ const tvr_dashboard = () => {
     {/* <Navbar/> */}
     <p>Welcome back Admin</p>
     <div className="row">
-  <div className="col-sm-4 mb-3 mb-3">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">MTD- Total Intrested</h5>
-        <p className='fs-4'>10</p>
-      </div>
-    </div>
-  </div>
-  <div className="col-sm-4 mb-3 mb-3">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">MTD- Total Not-Intrested</h5>
-        <p className='fs-4'>10</p>
-
-      </div>
-    </div>
-  </div>
   <div className="col-sm-4 mb-3">
     <div className="card">
       <div className="card-body">
