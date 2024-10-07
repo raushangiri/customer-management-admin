67f12ccd962804cc20d3bb830b6f8b6c945b2f6c
Raushan kumar giri <raushan.giri@orangecurrent.com>
2024-10-07 08:20:19 +0100

    search fix and attachment upload fix

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ import Adminsearch from './Component/Components/Adminsearch';
 import Teamleader_dashboard from './Component/Dashboard/Teamleader_dashboard';
 import Adminfilehistory from './Component/History/adminfilehistory';
 import Teamperformance from './Component/Team_Leader/Teamperformance';
-// import Teamleaderhistory from './Component/History/Teamleaderhistory';
+import Teamleaderhistory from './Component/History/Teamleaderhistory';
 import Adminsaleshistory from './Component/History/Adminsaleshistory';
 import Admintvrhistory from './Component/History/Admintvrhistory';
 import Admincdrhistory from './Component/History/Admincdrhistory';
@@ -106,7 +106,7 @@ function Main() {
         <Route path="/Adminsearch" element={<Adminsearch />} />
         <Route path="/Adminfilehistory" element={<Adminfilehistory />} />
         <Route path="/Adminsearch/:customer_mobilenumber" element={<Adminsearch />} />
-        {/* <Route path="/teamleaderfilehistory" element={<TeamleaderHistory />} /> */}
+        <Route path="/teamleaderfilehistory" element={<Teamleaderhistory />} />
         <Route path="/team-performance" element={<Teamperformance/>} />
         <Route path="/adminsaleshistory" element={<Adminsaleshistory/>} />
         <Route path="/admintvrhistory" element={<Admintvrhistory/>} />
