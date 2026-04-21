9ca6f976e466f554f9d51032ab3f541afa7a8c40
raushangiri <raushangiri.raj@gmail.com>
2026-04-21 14:56:30 +0530

    user available, chat, cdr history updated

--- src/Component/Chat/Chat.js
+++ src/Component/Chat/Chat.js
@@ -1348,7 +1348,7 @@ import "./ChatScreen.css";
 
 const reactbaseurl = process.env.REACT_APP_API_BASE_URL;
 // const socket = io("http://localhost:3009");
-const socket = io(reactbaseurl);
+const socket = io("http://82.180.147.224:3007");
 
 const ChatScreen = () => {
 
